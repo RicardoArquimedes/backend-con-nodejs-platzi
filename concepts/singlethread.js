500b8ee10717882778bda7731865a96f76e30a8d
RicardoArquimedes <richardcqt28@gmail.com>
2022-11-14 20:52:06 -0500

    :construction: adding singlethread file & creating directory for initial concepts

---
+++ concepts/singlethread.js
@@ -0,0 +1,14 @@
+console.log("Hello World!");
+
+let i = 0;
+
+setInterval(function(){
+    console.log(i);
+    console.log("Still Active")
+    
+    i++;
+    if (i === 5){
+        console.log("error here");
+        var a = 3 + z;
+    }
+}, 1000);
